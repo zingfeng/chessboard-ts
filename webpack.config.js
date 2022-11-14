96f974f8798291eb4ad015933e07ba2054cadb02
datbt-2563 <bui.thanh.dat@sun-asterisk.com>
2022-11-14 14:07:14 +0700

    add style

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
     ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: [".tsx", ".ts", ".js", ".css"],
   },
   // mode: "production",
   mode: "development",
